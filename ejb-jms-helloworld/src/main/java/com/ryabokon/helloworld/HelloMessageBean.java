731211d8929694fd8e8f8e0e556b4f93d347ffd5
archie-swif <artem.ryabokon@gmail.com>
2014-12-05 13:55:45 -0800

    Fixed EJB JMS app

--- ejb-jms-helloworld/src/main/java/com/ryabokon/helloworld/HelloMessageBean.java
+++ ejb-jms-helloworld/src/main/java/com/ryabokon/helloworld/HelloMessageBean.java
@@ -5,33 +5,15 @@ import javax.ejb.MessageDriven;
 import javax.jms.JMSException;
 import javax.jms.Message;
 import javax.jms.MessageListener;
-import javax.jms.TextMessage;
 
-@MessageDriven(mappedName = "jms/activemq/hello", activationConfig = {
-		@ActivationConfigProperty(propertyName = "acknowledgeMode", propertyValue = "Auto-acknowledge"),
-		@ActivationConfigProperty(propertyName = "destinationType", propertyValue = "javax.jms.Queue"),
-		@ActivationConfigProperty(propertyName = "subscriptionDurability", propertyValue = "Durable"),
-		@ActivationConfigProperty(propertyName = "clientId", propertyValue = "clientID"),
-		@ActivationConfigProperty(propertyName = "subscriptionName", propertyValue = "subscriberID") })
+@MessageDriven(activationConfig = { @ActivationConfigProperty(propertyName = "destinationType", propertyValue = "javax.jms.Queue"),
+		@ActivationConfigProperty(propertyName = "destination", propertyValue = "jms/queue/incoming") })
 public class HelloMessageBean implements MessageListener {
-
-	public HelloMessageBean() {
-	}
-
-	private int count = 0;
 
 	public void onMessage(Message message) {
 		try {
-			if (message instanceof TextMessage) {
-				TextMessage txtMsg = (TextMessage) message;
-				System.out.println("Received text message " + count + " from activeMQ:\n" + txtMsg.getText());
-			} else {
-				System.out.println("Received a message, it just isnt a textMessage!");
-			}
-		} catch (JMSException jmse) {
-			System.out.println("There was a error with the JMS");
-			jmse.printStackTrace();
-		} catch (Exception e) {
+			System.out.println("Weve received a message: " + message.getJMSMessageID());
+		} catch (JMSException e) {
 			e.printStackTrace();
 		}
 	}
